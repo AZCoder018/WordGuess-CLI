58a36cd86cf20b9d21aa7cc42b4bf4eefcddc62f
AZCoder018 <jason.meadows.esq@hotmail.com>
2018-04-19 21:23:29 -0700

    more revisions to index.js

--- index.js
+++ index.js
@@ -23,9 +23,31 @@ var gameStart = function() {
 
 /* Function that runs the game logic; prompts user for input; validate if input is a letter of alphabet
 validates if letter has already been chosen; check if letter is in the word */
-var turn = function() 
+var turn = function() {
+    inquirer.prompt({type: "input", message: "Guess a letter!", name: "guess"}).then(function(response) {
+    if (word.alphabet.indexOf(response.guess.toLowerCase()) === -1) {
+        console.log("Please chose a proper letter");
+        turn();
+    } else {
+    if (guessLetters.indexOf(response.guess.toLowerCase()) === -1) {
+    if (game.letters.indexOf(repsonse.guess.toLowerCase()) === -1) {
+        lives = lives -1;
+        guessedLetters.push(response.guess.toLowerCase());
+        console.log("Sorry, that letter is not in the word");
+        console.log("Current lives: " + lives);
 
-}
+//Checks how many lives are available, if so next turn
+    if (lives > 0) {
+        game.returnWord();
+        turn();
+
+//If not, game over
+    } else {
+        console.log("You lose!");
+        console.log("");
+        restart();
+
+
 
 //Start screen and start game
 console.log("\n---==== WORLD CAPITALS ====---\n");
