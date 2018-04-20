d3005f73dc35856615c5a641bb894f0c937dab54
AZCoder018 <jason.meadows.esq@hotmail.com>
2018-04-19 20:40:09 -0700

    start index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+//Import files; import npm modules
+var word = require("./Word.js");
+var inquirer = require("inquirer");
+
+//Variables
+var lives = 10;
+var guessedLetters = [];
+var game = "";
+
+//Word bank
