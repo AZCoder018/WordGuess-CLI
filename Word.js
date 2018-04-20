d120e41366efaf27e3c6cf82c71483bffbb2358d
AZCoder018 <jason.meadows.esq@hotmail.com>
2018-04-19 20:23:06 -0700

    revisions to Word.js

--- Word.js
+++ Word.js
@@ -5,5 +5,21 @@ var Letter = require("./Letter.js");
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
                 "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+//Word constructor; Need to split current word into array; store blank word generated from letters; reconstruct word from letters
+var Word = function(currentWord) {
+    this.letters = currentWord.split("");
+    this.newLettersArray = [];
+    this.currentIteration = "";
+}
+
+//Function to run guessed letter in the letter constructor
+this.guess = function(guessedLetter) {
+    for (var i = 0; i < this.newLettersArray.length; i++) {
+        this.newLettersArray[i].guessCheck(guessedLetter);
+        }
+    };
+};
+
 //Exports constructor and alphabet
 module.exports = {word: Word, alphabet: alphabet};
+};
