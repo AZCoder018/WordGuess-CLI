f80ce3824550250835f19719bd9bdbea02b53917
AZCoder018 <jason.meadows.esq@hotmail.com>
2018-04-19 21:04:13 -0700

    add to index.js

--- index.js
+++ index.js
@@ -8,3 +8,5 @@ var guessedLetters = [];
 var game = "";
 
 //Word bank
+var wordBank = ["bujumbura", "santo domingo", "ulaanbaatar", "wellington", "brazzaville", "kingston", "reykjavik", "vilnius", "jakarta", "islamabad",
+                "abu dhabi", "montevideo", "khartoum", "ottawa", "stockholm", "bratislava", "ljubljana", "bucharest", "pyongyang", "kuala lumpur"];
